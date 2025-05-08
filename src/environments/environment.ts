46c34c8f62486843e9555c76628813cbcd7505ca
Kowa77 <estebangarriga@hotmail.com>
2025-05-08 03:12:06 -0300

    ajustes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,11 @@
-// src/environments/environment.ts
-// Configuración de Firebase (Reemplaza con tus propias credenciales)
+
 export const environment = {
-  production: true,
+  production: false,
   firebase: {
-    apiKey: "AIzaSyAzcdECH5fK13xqZ_vwjKT1b7kg7TFbuVk",
+    apiKey: "AIzaSyAzcdECH5fK13xqZ_vwjKT1b7kg7TFbuVk", // ¡Asegúrate de que sea EXACTAMENTE igual!
     authDomain: "prueba-b4e16.firebaseapp.com",
-    databaseURL: "https://prueba-b4e16-default-rtdb.firebaseio.com/", // ¡Aquí actualiza la URL!https://prueba-b4e16-default-rtdb.firebaseio.com/
     projectId: "prueba-b4e16",
+    databaseURL: "https://prueba-b4e16-default-rtdb.firebaseio.com",
     storageBucket: "prueba-b4e16.firebasestorage.app",
     messagingSenderId: "570563896774",
     appId: "1:570563896774:web:8273007c65b27e22697ec7",
