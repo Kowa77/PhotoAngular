8ab8be5927ac5f07f2a6437b9e770634d4031494
kowa77 <estebangarriga@hotmail.com>
2025-08-21 02:58:19 -0300

    pequeños cambios

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 
 import express from 'express';
-import cors  from 'cors';  
+import cors  from 'cors';
 
 //SDK de Mercado Pago
 import { MercadoPagoConfig, Preference } from 'mercadopago';
 // Agregando credenciales
-const client = new MercadoPagoConfig({ accessToken: 'APP_USR-3150749082048161-051022-6e2dbe6b3a14156c7f1017fe26159f42-391606738' });
+const client = new MercadoPagoConfig({ accessToken: 'APP_USR-2512641808428898-051222-d1c09ec2d6085bb498be7ef0c4dacf14-55966984' });
 
 const app = express();
 const PORT = 3000;
@@ -35,18 +35,18 @@ app.post('/create_preference', async (req, res) => {
                 pending: "https://www.elpais.com.uy/"
             },
             auto_return: "approved",
-        };  
+        };
         const preference = new Preference(client);
         const result = await preference.create({body});
         res.json({
             id: result.id,
-        }); 
+        });
     } catch (error) {
         console.error('Error creating preference:', error);
         res.status(500).json({ error: 'Error al crear la preferencia ;(' });
-        return;       
+        return;
     }
-}); 
+});
 
 
 app.listen(PORT, () => {
